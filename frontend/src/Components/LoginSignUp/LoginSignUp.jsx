8f3776fd9287f62afaae7bd24b4a316ee45106d4
Devanshi gupta <dgkv0503@gmail.com>
2024-06-15 23:17:49 +0530

    LoginSignUp page

---
+++ frontend/src/Components/LoginSignUp/LoginSignUp.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './LoginSignUp.css'
+const LoginSignUp = () => {
+  return (
+    <div className='login_sign_up.jsx'>
+      
+    </div>
+  )
+}
+
+export default LoginSignUp
